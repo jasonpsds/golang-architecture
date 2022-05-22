fdcfcb57fa55df9a1cd7470c88a5d8702f1239e0
Jason <jason@psds-uk.net>
2022-05-22 16:27:00 +0100

    Video 36 improve code

--- Section 6 Interfaces/decoupling/main.go
+++ Section 6 Interfaces/decoupling/main.go
@@ -15,7 +15,7 @@ func (m mongo) save(n int, p person) {
 	m[n] = p
 }
 
-func (m mongo) receive(n int) person {
+func (m mongo) retrieve(n int) person {
 	return m[n]
 }
 
@@ -23,14 +23,26 @@ func (pg postg) save(n int, p person) {
 	pg[n] = p
 }
 
-func (pg postg) receive(n int) person {
+func (pg postg) retrieve(n int) person {
 	return pg[n]
 }
 
 // accessor interface
 type accessor interface {
 	save(n int, p person)
-	receive(n int) person
+	retrieve(n int) person
+}
+
+type personService struct {
+	a accessor
+}
+
+func (ps personService) get(n int) (person, error) {
+	p := ps.a.retrieve(n)
+	if p.first == "" {
+		return person{}, fmt.Errorf("No person with n of %d", n)
+	}
+	return p, nil
 }
 
 func put(a accessor, n int, p person) {
@@ -38,7 +50,7 @@ func put(a accessor, n int, p person) {
 }
 
 func get(a accessor, n int) person {
-	return a.receive(n)
+	return a.retrieve(n)
 }
 
 func main() {
@@ -52,6 +64,10 @@ func main() {
 	}
 	p2 := person{
 		first: "Jacinda",
+	}
+
+	ps := personService{
+		a: dbm,
 	}
 
 	// store data in the db
@@ -68,6 +84,9 @@ func main() {
 	fmt.Println(get(dbm, 1))
 	fmt.Println(get(dbm, 2))
 
+	fmt.Println(ps.get(1))
+	fmt.Println(ps.get(2))
+	fmt.Println(ps.get(3)) // error
 	// postg
 	fmt.Println(get(dbp, 1))
 	fmt.Println(get(dbp, 2))
